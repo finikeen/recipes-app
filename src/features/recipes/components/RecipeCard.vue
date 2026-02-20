a3ab5ece3c4529db0e6475c6512760d2771175c7
brian.cooney <brian.cooney@sinclair.edu>
2026-02-20 09:35:05 -0500

    feat: add placeholder RecipeCard component using PrimeVue Card

---
+++ src/features/recipes/components/RecipeCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <Card class="w-full">
+    <template #title>{{ recipe.title ?? 'Untitled Recipe' }}</template>
+    <template #content>
+      <p class="text-muted-color">{{ recipe.description ?? 'No description.' }}</p>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import Card from 'primevue/card'
+
+defineProps({
+  recipe: {
+    type: Object,
+    required: true
+  }
+})
+</script>
