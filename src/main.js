fbb48b4556c1c1c8a20743fdcb5332942db1297f
brian.cooney <brian.cooney@sinclair.edu>
2026-02-20 09:33:43 -0500

    feat: wire up pinia, vue-router, and primevue in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
+import router from './router/index.js'
 import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark'
+    }
+  }
+})
+
+app.mount('#app')
