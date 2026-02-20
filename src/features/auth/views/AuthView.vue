cfc02100eef877d70d74a77d9c2fb4dbe0dc7938
brian.cooney <brian.cooney@sinclair.edu>
2026-02-20 15:30:46 -0500

    feat: add AuthView wrapping AuthenticationForm

---
+++ src/features/auth/views/AuthView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="auth-view">
+    <AuthenticationForm />
+  </div>
+</template>
+
+<script setup>
+import AuthenticationForm from "@/features/auth/components/AuthenticationForm.vue";
+</script>
+
+<style scoped>
+.auth-view {
+  @apply flex items-center justify-center min-h-[60vh] px-4;
+}
+</style>
