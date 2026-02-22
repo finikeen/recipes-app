174b2c2a6fba1f4917c634a531161d04387300ca
brian.cooney <brian.cooney@sinclair.edu>
2026-02-22 16:48:36 -0500

    style: add forge styling to navbar (brand, links)

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { useAuthStore } from "@/features/auth/store"
-import { useRouter } from "vue-router"
+import { useAuthStore } from "@/features/auth/store";
+import { useRouter } from "vue-router";
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -13,24 +13,31 @@ const handleLogout = async () => {
 
 <template>
   <nav class="navbar">
-    <RouterLink :to="{ name: 'home' }" class="navbar__brand">
-      Recipes
+    <RouterLink :to="{ name: 'home' }" class="navbar__brand forge__brand">
+      Recipe Forge
     </RouterLink>
     <div class="navbar__links">
-      <RouterLink :to="{ name: 'browse' }" class="navbar__link">
+      <RouterLink :to="{ name: 'browse' }" class="navbar__link forge__link">
         Browse
       </RouterLink>
-      <RouterLink :to="{ name: 'recipe-create' }" class="navbar__link">
+      <RouterLink
+        :to="{ name: 'recipe-create' }"
+        class="navbar__link forge__link"
+      >
         New Recipe
       </RouterLink>
       <button
         v-if="authStore.isAuthenticated"
-        class="navbar__link navbar__link--button"
+        class="navbar__link navbar__link--button forge__link"
         @click="handleLogout"
       >
         Sign Out
       </button>
-      <RouterLink v-else :to="{ name: 'auth' }" class="navbar__link">
+      <RouterLink
+        v-else
+        :to="{ name: 'auth' }"
+        class="navbar__link forge__link"
+      >
         Sign In
       </RouterLink>
     </div>
